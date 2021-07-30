c4845c5f0b4c48c78cb40f0eceae3acf612a34d3
Endres <andresdhr99@gmail.com>
2021-07-30 03:58:57 -0400

    Added back end routes to product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,34 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async(req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const list_products = await Product.findAll({  include: [{ model: Category },{model:Tag, through:ProductTag, as: 'product_tags'}]});
+    console.log("Request made");
+    res.status(200).json(list_products);
+    console.log("Request sent");
+  } catch (err) {
+    console.log("error ran")
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async(req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const singleProduct = await Product.findByPk(req.params.id,
+      {  include: [{ model: Category },{model:Tag, through:ProductTag, as: 'product_tags'}]});
+    console.log("Request made");
+    res.status(200).json(singleProduct);
+    console.log("Request sent");
+  } catch (err) {
+    console.log("error ran")
+    res.status(500).json(err);
+  }
 });
 
 // create new product
